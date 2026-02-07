5c5c9a6fbad5d86d2344bf95d85da7d61f32fea3
jniermann <jrniermann@outlook.de>
2026-02-07 08:16:22 +0100

    add setup auth0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '@auth0/auth0-angular';
+import { LoginButton } from './login-button/login-button';
+import { LogoutButton } from './logout-button/logout-button';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule],
+  imports: [AsyncPipe, LoginButton, LogoutButton, MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Fauna';
+  protected auth = inject(AuthService);
 }
